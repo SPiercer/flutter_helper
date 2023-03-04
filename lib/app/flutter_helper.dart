61527011a8890e1354a3be0ea087cace9b0bad9e
SPiercer <snowpiercer2001@gmail.com>
2023-03-04 18:00:20 +0200

    initial app

---
+++ lib/app/flutter_helper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterHelper extends StatelessWidget {
+  const FlutterHelper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_helper/app/flutter_helper.dart';
+
+void main() {
+  runApp(const FlutterHelper());
+}
